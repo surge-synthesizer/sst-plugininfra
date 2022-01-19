c7e00db994e09887712946c490a80e68bd9b29b9
Paul <baconpaul@users.noreply.github.com>
2022-01-19 18:47:41 -0500

    FileSystem; TIXML Surge Changes (#5)
    1. Move the surge filesystem strategy here
    2. Port the Surge tixml changes
    	- works on fs::path
    	- internationalized doubles

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,12 +2,21 @@
 #include "catch2/catch2.hpp"
 
 #include <sst/plugininfra.h>
+#include "filesystem/import.h"
 
 TEST_CASE("Basic")
 {
-    SECTION("Base Test")
+    SECTION("Base Test") { REQUIRE(sst::plugininfra::addOne(1) == 2); }
+}
+
+TEST_CASE("FileSystem")
+{
+    SECTION("Can make a path")
     {
-        REQUIRE(sst::plugininfra::addOne(1) == 2);
+        auto p = fs::path{"tmp"} / fs::path{"var"};
+        auto s = p.u8string();
+        s[3] = ' ';
+        REQUIRE(s == "tmp var");
     }
 }
 
