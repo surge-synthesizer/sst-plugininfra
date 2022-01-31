5c4e8e464539f3fd6984db989c5e01fe88496817
Paul Walker <paul@pwjw.com>
2022-01-31 17:46:58 -0500

    Move the Surge FTZ test here
    For the CPU move

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -15,13 +15,37 @@ TEST_CASE("FileSystem")
     }
 }
 
-TEST_CASE( "Documents Folder" )
+TEST_CASE("Documents Folder")
 {
-    SECTION( "Got a documents folder" )
+    SECTION("Got a documents folder")
     {
         auto dpath = sst::plugininfra::paths::bestDocumentsFolderPathFor("surge-test");
         std::cout << dpath.u8string() << std::endl;
         REQUIRE(true);
+    }
+}
+
+TEST_CASE("FTZ", "[dsp]")
+{
+    auto g = sst::plugininfra::cpufeatures::FPUStateGuard();
+
+    unsigned char min_float[4] = {0x00, 0x00, 0x80, 0x00};
+    float f_min;
+    memcpy(&f_min, min_float, 4);
+    REQUIRE(f_min != 0.f);
+
+    unsigned char res_float[4];
+    for (int i = 0; i < 100; ++i)
+    {
+        // don't let the compiler optimize me away!
+        auto r = 1.0 / (fabs(rand() % 10) + 1.1); // a number < 1
+        float ftz_float = f_min * r;
+        memcpy(res_float, &ftz_float, 4);
+        for (int i = 0; i < 4; ++i)
+        {
+            REQUIRE(res_float[i] == 0);
+        }
+        REQUIRE(ftz_float == 0.f);
     }
 }
 
