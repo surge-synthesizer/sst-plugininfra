add2600ecdea8fbd740498f86cd5045c86938784
Mario Kruselj <mario.kruselj@gmail.com>
2023-10-23 15:26:05 +0200

    Another copy paste typo bleh

--- include/sst/plugininfra/userdefaults.h
+++ include/sst/plugininfra/userdefaults.h
@@ -40,7 +40,7 @@ template <typename E, int maxE> struct Provider
     }
 
     std::string getUserDefaultValue(const E &key, const std::string &valueIfMissing,
-                                    bool potentiallyRead = true, bool potentiallyRead = true)
+                                    bool potentiallyRead = true)
     {
         if (userPrefOverrides.find(key) != userPrefOverrides.end())
         {
