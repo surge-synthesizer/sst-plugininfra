de3106bedf83a2ae556337e818dc55c411510753
baconpaul <paul@pwjw.com>
2024-03-26 00:04:54 -0400

    move to miniz

--- tests/ziptests.cpp
+++ tests/ziptests.cpp
@@ -13,8 +13,9 @@
  */
 
 #include <iostream>
+#include <cstdint>
 
-#include <zip.h>
+#include <miniz.h>
 
 #include "catch2/catch2.hpp"
 #include "filesystem/import.h"
@@ -30,10 +31,27 @@ TEST_CASE("Can Unzip File")
     auto zipIn = p / "tests" / "three_cpp_files.zip";
     std::cout << "Reading rrom " << zipIn.u8string() << std::endl;
 
-    int err;
-    auto *zip = zip_open(zipIn.u8string().c_str(), ZIP_RDONLY, &err);
-    REQUIRE(zip);
 
-    auto entries = zip_get_num_entries(zip, 0);
+    mz_zip_archive zip_archive;
+    memset(&zip_archive, 0, sizeof(zip_archive));
+
+    auto status = mz_zip_reader_init_file(&zip_archive, zipIn.u8string().c_str(), 0);
+    REQUIRE(status);
+
+    auto entries = (int)mz_zip_reader_get_num_files(&zip_archive);
     REQUIRE(entries == 3);
+
+    for (int i = 0; i < (int)mz_zip_reader_get_num_files(&zip_archive); i++)
+    {
+        mz_zip_archive_file_stat file_stat;
+        REQUIRE(mz_zip_reader_file_stat(&zip_archive, i, &file_stat));
+
+        if (i == 0)
+            REQUIRE(std::string(file_stat.m_filename) == "dumppaths.cpp");
+        if (i == 1)
+            REQUIRE(std::string(file_stat.m_filename) == "tests.cpp");
+        if (i == 2)
+            REQUIRE(std::string(file_stat.m_filename) == "tests_linux.cpp");
+    }
+
 }
