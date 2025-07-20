6769729ebc2caaa676cd8bca341df7dbd1cc9a48
Paul <baconpaul@users.noreply.github.com>
2025-07-20 08:46:51 -0400

    comment only change - correct header (#60)
    the header refered incorrectly to sst-effects in one place, so
    fix it, and update copyright year while in there.

--- scripts/fix_file_comments.pl
+++ scripts/fix_file_comments.pl
@@ -43,10 +43,10 @@ sub findfiles
  * sst-plugininfra - an open source library of plugin infrastructure
  * built by Surge Synth Team.
  *
- * Copyright 2018-2024, various authors, as described in the GitHub
+ * Copyright 2018-2025, various authors, as described in the GitHub
  * transaction log.
  *
- * sst-effects is released under the MIT License. It has subordinate
+ * sst-plugininfra is released under the MIT License. It has subordinate
  * libraries with licenses as described in libs/
  *
  * All source in sst-plugininfra available at
